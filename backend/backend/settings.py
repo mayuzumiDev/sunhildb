d2c37062c604846de95fc977ba8cc215336362e2
mayuzumi1272 <117078422+mayuzumiDev@users.noreply.github.com>
2025-01-14 18:03:36 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sunhilldb',
-        'USER': 'sunhill_user',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
